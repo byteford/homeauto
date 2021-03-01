25dc04e791fa012683912f22f281d79ca9fbf5f1
James Sandford <james.sandford@ecs-digital.co.uk>
2021-03-01 09:14:01 +0000

    feat: create can handle a 201 return code

--- provider/client.go
+++ provider/client.go
@@ -113,7 +113,7 @@ func (c *Client) doRequest(req *http.Request) ([]byte, error) {
 		return nil, err
 	}
 
-	if res.StatusCode != http.StatusOK {
+	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusCreated {
 		return nil, fmt.Errorf("status: %d, body: %s", res.StatusCode, body)
 	}
 
