bdb8656ab0c7e086de3ef11b531552d5df5bde3c
James Sandford <sandford.james@gmail.com>
2021-07-29 09:16:38 +0100

    Updating build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 rm .terraform.lock.hcl || true
 cd provider
 
-go build -o ~/.terraform.d/plugins/github.com/$1/homeauto/$2/darwin_amd64/terraform-provider-homeauto_v$2
+# $1 is equal to NAME
+# $2 is equal to version number
+go build -o ~/.terraform.d/plugins/github.com/$1/homeauto/$2/linux_amd64/terraform-provider-homeauto_v$2
 
 echo "Built file"
 cd ..
